46f92477eac71378394ec5d41aef9732b347d3c1
Timothy Holmes <tpholmes7@gmail.com>
2023-10-18 11:27:38 -0500

    Initial Commit

---
+++ clepsydra/clepsydraApp.swift
@@ -0,0 +1,17 @@
+//
+//  clepsydraApp.swift
+//  clepsydra
+//
+//  Created by Tim Holmes on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct clepsydraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
